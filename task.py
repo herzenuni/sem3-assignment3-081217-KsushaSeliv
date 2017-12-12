b332066bbba977c752380fbe0c7c23b31484d993
KsushaSeliv <KsenijaSeliv@yandex.ru>
2017-12-12 10:30:52 +0300

    Update task.py

--- task.py
+++ task.py
@@ -29,3 +29,14 @@ pprint(data)
 # Вывести в форматированном виде поля: 
 
 # company, email, phone, address
+
+
+import pprint 
+import json 
+filename = 'data.json' 
+try: 
+with open(filename, encoding='utf-8') as data_file: 
+data = json.loads(data_file.read()) 
+except OSError: 
+print("404. Вы неправильно ввели имя") 
+pprint.pprint(data)
