b62169370c5a273b5f14cd84e3b5a5cddffb9aba
KsushaSeliv <KsenijaSeliv@yandex.ru>
2018-01-10 14:51:37 +0300

    Update task.py

--- task.py
+++ task.py
@@ -1,9 +1,26 @@
-import pprint 
-import json 
-filename = 'data.json' 
-try: 
- with open(filename, encoding='utf-8') as data_file: 
-  data = json.loads(data_file.read()) 
-except OSError: 
- print('Вы неправильно ввели имя файла ') 
+import pprint
+import json
+
+filename = 'data.json'
+
+try:
+    with open(filename, encoding='utf-8') as data_file:
+        json_file = open('data.json', 'r')
+        fl = json_file.read()
+        data = json.loads(fl)
+except FileExistsError:
+    print("Файл не найден!")
+
 pprint.pprint(data)
+
+# Вывести в форматированном виде поля: company, email, phone, address
+
+for i in enumerate(data):
+	dict = {}
+	dict.update(
+		{'company':i.get('company'),
+		 'email':i.get('email'),
+		 'phone':i.get('phone'),
+		 'address':i.get('address')})
+
+	pprint.pprint(dict)
